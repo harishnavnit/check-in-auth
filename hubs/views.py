1d9069599e0a957cd6a3a812e6b2a44448e1bb29
R. Harish Navnit <harishnavnit@gmail.com>
2018-05-27 18:41:22 +0530

    Maintain the hubs templates inside it's own templates subdir.

--- hubs/views.py
+++ hubs/views.py
@@ -1,5 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hubs page coming soon")
+    return render(request, 'hubs/location.html')
+
+
+def locations(request):
+    # _status = 200 if request.GET['coordinates'] else 500
+    # return HttpResponse(status=_status)
+    print("request : ", request)
+    return HttpResponse(status=200)
